dec26916facf25e71beac426126defd70b054438
Vincent <vincent.dagoury@gmail.com>
2022-08-20 13:24:56 +0200

    Fiwing Lambda Layer and code

--- cdk_unzip_lambda/cdk_unzip_lambda_stack.py
+++ cdk_unzip_lambda/cdk_unzip_lambda_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     aws_lambda as _lambda,
     aws_iam as _iam,
     CfnOutput,
+    RemovalPolicy,
     Stack,
 
 )
@@ -12,6 +13,7 @@ class CdkUnzipLambdaStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
 
         # Create role for your Lambda function
         lambda_role = _iam.Role(scope=self, id='cdk-lambda-role',
@@ -24,24 +26,32 @@ class CdkUnzipLambdaStack(Stack):
                                         'service-role/AWSLambdaBasicExecutionRole')
                                 ])
 
-        layer = _lambda.LayerVersion(self, 'UnzipFileFromS3BucketLayer',
-                                     code=_lambda.AssetCode('lambda/layer/'),
-                                     description='The principal layer of UnzipFileFromS3Bucket'
+        # create layer
+        layer = _lambda.LayerVersion(self, 'UnzipFileFromS3Bucket_layer',
+                                     code=_lambda.Code.from_asset("layer"),
+                                     description='Common helper utility',
+                                     compatible_runtimes=[
+                                         _lambda.Runtime.PYTHON_3_6,
+                                         _lambda.Runtime.PYTHON_3_7,
+                                         _lambda.Runtime.PYTHON_3_8,
+                                         _lambda.Runtime.PYTHON_3_9,
+                                     ],
+                                     removal_policy=RemovalPolicy.DESTROY
                                      )
-
+        # create lambda function
         cdk_lambda = _lambda.Function(
             self,
             'UnzipFileFromS3Bucket',
             runtime=_lambda.Runtime.PYTHON_3_9,
-            code=_lambda.AssetCode('lambda/code'),
+            code=_lambda.Code.from_asset("lambda"),
             description='Lambda function to unzip a file from an S3 bucket. Lambda is triggered by S3 event.',
             handler="unzip_file_from_s3.handler",
             role=lambda_role,
+            layers=[layer],
             environment={
                 'DESTINATION_BUCKET': f'{Stack.of(self).account}-destination-bucket'
             }
         )
-        cdk_lambda.add_layers(layer)
 
         # Output of created resource
         CfnOutput(scope=self, id='cdk-output-lambda',
