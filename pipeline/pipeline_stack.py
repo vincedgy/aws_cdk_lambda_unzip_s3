e6564af2abc76fb3fb9b81be8c2912caf679bc6a
Vincent <vincent.dagoury@gmail.com>
2022-08-21 13:22:10 +0200

    Fixing Pipeline name (recreate)

--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -1,62 +1,41 @@
-from constructs import Construct
 from aws_cdk import (
     aws_codecommit as codecommit,
     pipelines as pipelines,
     Stack,
 )
+from constructs import Construct
+
 from pipeline.pipeline_stage import UnzipLambdaToS3PipelineStage
 
 
 class UnzipLambdaToS3PipelineStack(Stack):
-      
+
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Creates a CodeCommit repository called 'WorkshopRepo'
-        repo = codecommit.Repository(
-            self, 'UnzipFileToS3Repo',
-            repository_name="UnzipFileToS3Repo"
-        )
+        # Creates a CodeCommit repository called 'UnzipFileToS3Repo'
+        repo = codecommit.Repository.from_repository_name(self, 'UnzipFileToS3Repo',
+                                                          repository_name="UnzipFileToS3Repo")
+        # repo = codecommit.Repository(
+        #    self, 'UnzipFileToS3',
+        ##    repository_name="UnzipFileToS3Repo"
+        # )
 
         # Pipeline code will go here
-        pipeline = pipelines.CodePipeline(
-            self,
-            "UnzipFileToS3Pipeline",
-            synth=pipelines.ShellStep(
-                "Synth",
-                input=pipelines.CodePipelineSource.code_commit(repo, "main"),
-                commands=[
-                    "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
-                    # Instructs Codebuild to install required packages
-                    # "pip install --upgrade pip",
-                    "pip install -r requirements.txt",
-                    "cdk synth",
-                ]
-            ),
-        )
+        pipeline = pipelines.CodePipeline(self, "UnzipFileToS3Pipeline",
+                                          synth=pipelines.ShellStep(
+                                              "Synth",
+                                              input=pipelines.CodePipelineSource.code_commit(repo, "main"),
+                                              commands=[
+                                                  "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
+                                                  # Instructs Codebuild to install required packages
+                                                  # "pip install --upgrade pip",
+                                                  "pip install -r requirements.txt",
+                                                  "cdk synth",
+                                              ]
+                                          )
+
+                                          )
 
         deploy = UnzipLambdaToS3PipelineStage(self, "Deploy")
-        deploy_stage = pipeline.add_stage(deploy)
-
-        # deploy_stage.add_post(
-        #   pipelines.ShellStep(
-        #       "TestViewerEndpoint",
-        #       env_from_cfn_outputs={
-        #           "ENDPOINT_URL": deploy.hc_viewer_url
-        #       },
-        #       commands=["curl -Ssf $ENDPOINT_URL"],
-        #   )
-        # )
-        # deploy_stage.add_post(
-        #     pipelines.ShellStep(
-        #         "TestAPIGatewayEndpoint",
-        #         env_from_cfn_outputs={
-        #             "ENDPOINT_URL":  deploy.hc_endpoint
-        #         },
-        #         commands=[
-        #             "curl -Ssf $ENDPOINT_URL",
-        #             "curl -Ssf $ENDPOINT_URL/hello",
-        #             "curl -Ssf $ENDPOINT_URL/test",
-        #         ],
-        #     )
-        # )
+        pipeline.add_stage(deploy)
